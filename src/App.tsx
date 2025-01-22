35d8eac96b92968ee49cce37d96659f0bef1969f
Vansh Kanojia <117327847+iTsThermo@users.noreply.github.com>
2025-01-22 00:26:11 -0500

    Removed analytics from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+console.log(app);
 
 function App() {
     const [mode, setMode] = useState<string>('dark');
